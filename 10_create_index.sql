0ea901e41f4f193c7b7a6fff0e88f00bd0828a9e
Ruddy Setiadi Gunawan <ntcv_mr68@outlook.com>
2025-02-27 13:34:11 -0500

    Include the schema name to the index creation

--- 10_create_index.sql
+++ 10_create_index.sql
@@ -3,7 +3,7 @@
 -- =======================================
 
 -- This script is simple, it creates an index on the 'email' column of the 'customer' table
-CREATE INDEX idx_customer_email ON customer(email);
+CREATE INDEX idx_customer_email ON public.customer(email);
 
 -- To test the execution time difference, run the following statement before and after creating the index:
 -- EXPLAIN ANALYZE
